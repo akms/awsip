6206bf06bc3b3ff3f635e3c5d7ada4d9851c17f1
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-02-12 09:59:22 +0900

    first commit

---
+++ awsjson.go
@@ -0,0 +1,90 @@
+package awsip
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type AwsJson struct {
+	SyncToken  string              `json:syncToken`
+	CreateDate string              `json:createDate`
+	Prefixes   []map[string]string `json:prefixes`
+	Headder    string
+}
+
+func NewAwsIpadder() *AwsJson {
+	var (
+		resp       *http.Response
+		json_body  []byte
+		err        error
+		json_url   string = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+		awsip_json AwsJson
+	)
+
+	resp, err = http.Get(json_url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json_body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp.Body.Close()
+	err = json.Unmarshal(json_body, &awsip_json)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &awsip_json
+}
+
+func (ip *AwsJson) GetIpadder(region string, service string) (ip_buffer *bytes.Buffer) {
+	if region == "" || service == "" {
+		var geterr error = fmt.Errorf("nil value region or service.\n region: %s, service: %s\n", region, service)
+		log.Fatal(geterr)
+	}
+	ip_buffer = bytes.NewBufferString("")
+	for _, val := range ip.Prefixes {
+		if val["region"] == region && val["service"] == service {
+			ip_buffer.WriteString(ip.Headder)
+			ip_buffer.WriteString(" ")
+			ip_buffer.WriteString(val["ip_prefix"])
+			ip_buffer.WriteString("\n")
+		}
+	}
+	return
+}
+
+func (ip *AwsJson) GetIpadderRegion(region string) (ip_buffer *bytes.Buffer) {
+	ip_buffer = bytes.NewBufferString("")
+	for _, val := range ip.Prefixes {
+		if val["region"] == region {
+			ip_buffer.WriteString(ip.Headder)
+			ip_buffer.WriteString(" ")
+			ip_buffer.WriteString(val["ip_prefix"])
+			ip_buffer.WriteString("\n")
+		}
+	}
+	return
+}
+
+func (ip *AwsJson) GetIpadderService(service string) (ip_buffer *bytes.Buffer) {
+	ip_buffer = bytes.NewBufferString("")
+	for _, val := range ip.Prefixes {
+		if val["service"] == service {
+			ip_buffer.WriteString(ip.Headder)
+			ip_buffer.WriteString(" ")
+			ip_buffer.WriteString(val["ip_prefix"])
+			ip_buffer.WriteString("\n")
+		}
+	}
+	return
+}
+
+func (ip *AwsJson) SetHeadder(head_string string) {
+	ip.Headder = head_string
+}
---
+++ awsjson_test.go
@@ -0,0 +1,63 @@
+package awsip
+
+import (
+	"testing"
+)
+
+func TestNewAwsIpadder(t *testing.T) {
+	var t_aws *AwsJson
+	aws := NewAwsIpadder()
+	if aws == t_aws {
+		t.Errorf("want type: %T\n get type: %T",aws,t_aws)
+	}
+	if aws.SyncToken == "" {
+		t.Errorf("want numeric string. example:1417984028")
+	}
+	if aws.CreateDate == "" {
+		t.Errorf("want date string. example:2014-12-07-20-31-01")
+	}
+	if aws.Prefixes == nil {
+		t.Errorf("want map array. example:[{ ip_prefix:xx.xx.xx.xx/yy, region:us-east-1, service:AMAZON }]")
+	}
+	if aws.Headder != "" {
+		t.Errorf("want nil.got %s",aws.Headder)
+	}
+}
+
+func TestGetIpadder(t *testing.T) {
+	aws := NewAwsIpadder()
+	var (
+		region string = aws.Prefixes[0]["region"]
+		service string = aws.Prefixes[0]["service"]
+	)
+	b := aws.GetIpadder(region,service)
+	if b == nil {
+		t.Errorf("want *bytes.Buffer value. got nil value.")
+	}
+}
+
+func TestGetIpadderRegion(t *testing.T) {
+	aws := NewAwsIpadder()
+	var region string = aws.Prefixes[0]["region"]
+	b := aws.GetIpadderRegion(region)
+	if b == nil {
+		t.Errorf("want *bytes.Buffer value. got nil value.")
+	}
+}
+
+func TestGetIpadderService(t *testing.T) {
+	aws := NewAwsIpadder()
+	var service string = aws.Prefixes[0]["service"]
+	b := aws.GetIpadderService(service)
+	if b == nil {
+		t.Errorf("want *bytes.Buffer value. got nil value.")
+	}
+}
+
+func TestSetHeadder(t *testing.T) {
+	aws := NewAwsIpadder()
+	aws.SetHeadder("hoge")
+	if aws.Headder != "hoge" {
+		t.Errorf("want hoge. got %s",aws.Headder)
+	}
+}
